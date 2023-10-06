5ec11943e66bf1970e3a9748d9a37440a1979393
Ismail Chbiki <is.chbiki@gmail.com>
2023-10-06 13:02:58 +0200

    SQL server connection string added

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -10,25 +10,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 
+// Configure the database based on the environment
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+    Console.WriteLine("--> Using Development settings");
+}
+else if (builder.Environment.IsProduction())
+{
+    var connectionString = builder.Configuration.GetConnectionString("PlatformDBConnection");
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString));
+    Console.WriteLine("--> Using Production settings");
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-    Console.WriteLine("--> Using Development settings");
-}
-else if (app.Environment.IsProduction())
-{
-    Console.WriteLine("--> Using Production settings");
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
