86a2123ba9b1740e567a5d9e368e6912bcde53cd
Ismail Chbiki <is.chbiki@gmail.com>
2023-10-19 17:22:05 +0200

    gRPC Client added to Commands Service

--- CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CommandsService.Dtos;
 using CommandsService.Models;
+using PlatformService;
 
 namespace CommandsService.Profiles
 {
@@ -14,6 +15,12 @@ namespace CommandsService.Profiles
             CreateMap<Command, CommandReadDto>();
             CreateMap<PlatformPublishedDto, Platform>()
                 .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.Id));
+            CreateMap<GrpcPlatformModel, Platform>()
+                // this line is necessary.
+                .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.PlatformId))
+                // These lines are not necessary. (used just as a precaution)
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Commands, opt => opt.Ignore());
         }
     }
 }
