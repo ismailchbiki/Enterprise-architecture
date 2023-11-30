cff1e55d6de85a04f334061be31d633ed5d5ae63
Ismail Chbiki <is.chbiki@gmail.com>
2023-11-30 23:28:10 +0100

    Testing Fontys infrastructure (K8S Cluster)

--- KiteschoolService/Program.cs
+++ KiteschoolService/Program.cs
@@ -1,6 +1,5 @@
 using KiteschoolService.Data;
 using KiteschoolService.AsyncDataServices;
-using KiteschoolService.Data;
 using KiteschoolService.EventProcessing;
 using KiteschoolService.SyncDataServices.Grpc;
 using Microsoft.EntityFrameworkCore;
@@ -15,12 +14,12 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IKiteschoolRepo, KiteschoolRepo>();
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
-builder.Services.AddHostedService<MessageBusSubscriber>();
+// builder.Services.AddHostedService<MessageBusSubscriber>();
 builder.Services.AddScoped<IKiteschoolDataClient, KiteschoolDataClient>();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Command Service API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Kiteschool Service API", Version = "v1" });
 });
 
 var app = builder.Build();
@@ -29,7 +28,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Command Service API V1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Kiteschool Service API V1");
 });
 
 app.UseHttpsRedirection();
@@ -37,6 +36,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 // Generate some data for the in-memory database
-PrepDb.PrepPopulation(app);
+// PrepDb.PrepPopulation(app);
 
 app.Run();
